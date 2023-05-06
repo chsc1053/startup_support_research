fe764216c21c7b09b50e330c854d9f913d39217c
CHSC-1053 <chsaichaitanya1@gmail.com>
2023-05-06 09:03:32 +0530

    updated scopus api files

--- scopus api/main.py
+++ scopus api/main.py
@@ -8,16 +8,15 @@ API_KEY = '046ebcb86c11856c2c9d978b0570109f'
 BASE_URL = 'https://api.elsevier.com/content/search/scopus'
 
 # User input
-# query = input('Enter your query: ')
-query = 'solar energy'
+query = input('Enter your query: ')
 
 # API request parameters
 params = {
     'apiKey': API_KEY,
     'query': query,
-    # 'field': 'dc:identifier,dc:title,dc:creator,dc:description,authkeywords',
-    'view': 'STANDARD',
-    'count': 1
+    'field': 'dc:identifier,dc:title,prism:coverDate,prism:aggregationType,subtypeDescription',
+    'count': 20
+    # 'view': 'STANDARD'
 }
 
 # API request
@@ -27,52 +26,70 @@ response = requests.get(BASE_URL, params=params)
 if response.status_code == 200:
     # Extract data from API response
     data = response.json()['search-results']['entry']
-    # print(data)
+    for i in data:
+        print(i,"\n\n")
 
     for i in range(len(data)):
         data[i].pop('@_fa')
         data[i].pop('prism:url')
-        data[i]['SCOPUS_ID'] = data[i].pop('dc:identifier')[10:]
-        data[i]['eid'] = data[i].pop('eid')
+        data[i]['Scopus ID'] = data[i].pop('dc:identifier')[10:]
         data[i]['Title'] = data[i].pop('dc:title')
-        data[i]['Creator'] = data[i].pop('dc:creator')
-        data[i]['Publication Name'] = data[i].pop('prism:publicationName')
-        data[i]['ISSN'] = data[i].pop('prism:issn')
-        data[i]['eISSN'] = data[i].pop('prism:eIssn')
-        data[i]['Volume'] = data[i].pop('prism:volume')
-        data[i]['Page Range'] = data[i].pop('prism:pageRange')
         data[i]['Cover Date'] = data[i].pop('prism:coverDate')
-        data[i]['Cover Display Date'] = data[i].pop('prism:coverDisplayDate')
-        data[i]['DOI'] = data[i].pop('prism:doi')
-        data[i]['pii'] = data[i].pop('pii')
-        data[i]['citedby-count'] = data[i].pop('citedby-count')
-        data[i]['Affiliation Name'] = data[i]['affiliation'][i]['affilname']
-        data[i]['Affiliation City'] = data[i]['affiliation'][i]['affiliation-city']
-        data[i]['Affiliation Country'] = data[i]['affiliation'][i]['affiliation-country']
-        data[i].pop('affiliation')
-        data[i]['Aggregation Type'] = data[i].pop('prism:aggregationType')
-        data[i]['Subtype'] = data[i].pop('subtype')
-        data[i]['Subtype Description'] = data[i].pop('subtypeDescription')
-        data[i]['Source ID'] = data[i].pop('source-id')
-        data[i]['Open Access'] = data[i].pop('openaccess')
-        data[i]['Open Access Flag'] = data[i].pop('openaccessFlag')
-        
-        abstract_link =  data[i]['link'][2]['@href']
-        print(abstract_link)
-
-        #fetch abstract from abstract_link and apeennd to data[i]
-
-        data[i].pop('link')
-    
-    print(data)
+        data[i]['Aggregation Type'] = data[i].pop('prism:aggregationType') 
+        data[i].pop('subtype')
+        data[i]['Subtype'] = data[i].pop('subtypeDescription')
+            
+    for i in data:
+        print(i,"\n\n")
 
     # Convert data to a Pandas DataFrame
     df = pd.DataFrame(data)
 
     # Save data to JSON file with indented format
-    df.to_json('output.json', orient='records', indent=4)
+    df.to_json('./output.json', orient='records', indent=4)
 
     # Save data to CSV file
-    df.to_csv('output.csv', index=False)
+    df.to_csv('./output.csv', index=False)
 else:
     print('Error:', response.status_code)
+
+
+'''
+Scopus Search Views
+
+STANDARD
+
+link ref=scopus - Scopus abstract detail page URL
+
+dc:identifier - Scopus ID
+
+dc:title - Article Title
+
+prism:aggregationType - Source Type
+
+subtypeDescription - Document Type description
+
+citedby-count - Cited-by Count
+
+prism:publicationName - Source Title
+
+prism:volume - Volume
+
+prism:coverDate - Publication Date(YYYY-MM-DD)
+
+dc:creator  - First Author
+
+openaccess - Open Access status
+
+affilname - Affiliation name
+
+affiliation-city - Affiliation city
+
+affiliation-country - Affiliation country
+
+COMPLETE
+
+dc:description - Abstract
+
+authkeywords - Author Keywords
+'''
